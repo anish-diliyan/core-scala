5d4d1aa423104178be33223ff0fd362a6da033ba
anish <anish.diliyan@gmail.com>
2024-05-04 14:51:06 +0530

    sorting added

---
+++ src/main/scala/collection/sorting/ErrorWithEmp.scala
@@ -0,0 +1,22 @@
+package collection.sorting
+
+/*
+ *  sorted: Sorts this sequence according to an default order.
+ *  Error:- No implicit Ordering defined for B, How to solve?
+ *  extend an Ordered trait and override compare method.
+ *  But according to error it was looking for implicit Ordering.
+ *  So Ordered companion object provide implicit orderingToOrdered.
+ */
+// Default ordering of Employee
+case class Employee(id: Int, sal: Long) extends Ordered[Employee] {
+  override def compare(that: Employee): Int = {
+    this.sal compare that.sal
+  }
+}
+object ErrorWithEmp extends App {
+  val e1 = Employee(1, 1000)
+  val e2 = Employee(3, 3000)
+  val e3 = Employee(2, 2000)
+  val emp = List(e1, e2, e3)
+  println(emp.sorted)
+}
---
+++ src/main/scala/collection/sorting/sorted.scala
@@ -0,0 +1,50 @@
+package collection.sorting
+
+
+object sorted extends App {
+  val list = List(
+    "Our", "fathers", "trusted", "in", "You", "They",
+    "trusted", "and", "You", "delivered", "them"
+  )
+  // sort the list using default sorting order.
+  println(list.sorted)
+
+  // sort a list by length of words
+  println(list.sortBy(x => x.length))
+
+  // sort a list using lowercase of words
+  println(list.sortBy(x => x.toLowerCase))
+
+  // sort by can also work with classes
+  case class Track(length: Double, name: String)
+
+  val ts = List(
+    Track(7.35, "Watcher of the Skies"),
+    Track(4.783, "Time Table"),
+    Track(8.583, "Get 'Em Out by Friday"),
+    Track(5.75, "Can-Utility and the Coastliners"),
+    Track(1.65, "Horizons"),
+    Track(22.95, "Supper's Ready")
+  )
+  // sort by track length
+  println(ts.sortBy(x => x.length))
+
+  /*
+   * But What if you need to sort Track by "Descending Order Using Length" ?????
+   */
+   println(ts.sortWith((obj1, obj2) => obj1.length > obj2.length))
+
+  case class Name(first: String, last: String)
+
+  val names = List(
+    Name("Phil", "Collins"), Name("Jackie", "Collins"),
+    Name("Joan", "Collins"), Name("Tom", "Collins"),
+    Name("George", "Bush"), Name("Jeb", "Bush"), Name("Neil", "Bush"), Name("Marvin", "Bush")
+  )
+
+  // sort according to lastName and then first name by length desc
+  println(names.sortWith((name1, name2) => 
+    if(name1.last == name2.last) name1.first.length > name2.first.length
+    else name1.last < name2.last
+  ))
+}
