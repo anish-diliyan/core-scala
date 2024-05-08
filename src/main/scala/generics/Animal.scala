5cdab711d58ac75fc7b6c4d730f6eae0cbed9c7b
anish <anish.diliyan@gmail.com>
2024-05-09 05:29:08 +0530

    animal generics updated

--- src/main/scala/generics/Animal.scala
+++ src/main/scala/generics/Animal.scala
@@ -1,10 +1,6 @@
 package generics
 
-abstract class Animal {
-  def name: String
-}
-
-abstract class Pet extends Animal
-
-case class Cat(name: String) extends Pet
-case class Dog(name: String) extends Pet
+class Animal
+class Pet extends Animal
+class Cat extends Pet
+class Dog extends Pet
